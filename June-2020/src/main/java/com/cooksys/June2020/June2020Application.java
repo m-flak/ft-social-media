6509df43209a7921b1b8d58fd701e82e19a20bcb
Marshall <43308670+mselvidge@users.noreply.github.com>
2020-05-13 09:38:08 -0500

    June-2020 initialized db

---
+++ June-2020/src/main/java/com/cooksys/June2020/June2020Application.java
@@ -0,0 +1,13 @@
+package com.cooksys.June2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class June2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(June2020Application.class, args);
+	}
+
+}
---
+++ June-2020/src/test/java/com/cooksys/June2020/June2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.June2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class June2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
