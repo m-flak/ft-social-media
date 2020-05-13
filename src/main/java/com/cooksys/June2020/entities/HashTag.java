c15b008fed3671b7eeca1f45858c00cfaf760f61
Matthew Kehrer <matt91.mek@gmail.com>
2020-05-13 14:42:32 -0500

    Implemented the HashTag Entity. Closes #5

---
+++ src/main/java/com/cooksys/June2020/entities/HashTag.java
@@ -0,0 +1,35 @@
+package com.cooksys.June2020.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "hashtags")
+@NoArgsConstructor
+@Getter
+@Setter
+public class HashTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    // HashTag parsing from a tweet's contents should lowercase the tags when tagging
+    // Since all hashtags will be converted to lowercase when contents are parsed, they'll be unique!
+    @Column(unique = true, nullable = false)
+    private String label;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Timestamp firstUsed;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Timestamp lastUsed;
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "hashtags")
+    private List<Tweet> tweets;
+}
