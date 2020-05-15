e09e7d9dbc067cf0010b4545101372a275317a42
Edward Morris <ekmorris@outlook.com>
2020-05-15 08:26:13 -0400

    Created RestExceptionHandler. Updated TweetController, TweetService. Close #49

--- src/main/java/com/cooksys/June2020/controllers/TweetController.java
+++ src/main/java/com/cooksys/June2020/controllers/TweetController.java
@@ -1,9 +1,14 @@
 package com.cooksys.June2020.controllers;
 
-import com.cooksys.June2020.services.TweetService;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.June2020.dtos.TweetRequestDto;
+import com.cooksys.June2020.dtos.TweetResponseDto;
+import com.cooksys.June2020.services.TweetService;
 
 @RestController
 @RequestMapping("/tweets")
@@ -15,4 +20,10 @@ public class TweetController {
 		this.tweetService = tweetService;
 	}
 
+	@PostMapping
+	public ResponseEntity<TweetResponseDto> createTweet(@RequestBody TweetRequestDto tweet) {
+		return tweetService.createTweet(tweet);
+	}
+
 }
+
