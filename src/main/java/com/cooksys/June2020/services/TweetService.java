b8678492c51796069940d7c4e95d54cbc767c17a
Edward Morris <ekmorris@outlook.com>
2020-05-15 09:01:54 -0400

    Updated TweetRepository

--- src/main/java/com/cooksys/June2020/services/TweetService.java
+++ src/main/java/com/cooksys/June2020/services/TweetService.java
@@ -2,7 +2,15 @@ package com.cooksys.June2020.services;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksys.June2020.repositories.TweetRepository;
+
 @Service
 public class TweetService {
+
+	private TweetRepository tweetRepository;
+
+	public TweetService(TweetRepository tweetRepository) {
+		this.tweetRepository = tweetRepository;
+	}
 
 }
