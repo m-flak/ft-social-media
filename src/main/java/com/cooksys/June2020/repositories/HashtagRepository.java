c486b84a943da9b22edab75b2f82bf5efe54acef
Marshall <43308670+mselvidge@users.noreply.github.com>
2020-05-15 07:20:03 -0500

    Add files via upload
    JpaRepository<Hashtag, Long> to JpaRepository<Hashtable, Long> would remove the error in line 17.
    JpaRepository<HashTag, Long> written to follow syntax of ChatApp.
    JpaRepository<HashTag, Long> and not JpaRepository<Hashtag, Long> written to follow syntax in entities package.

---
+++ src/main/java/com/cooksys/June2020/repositories/HashtagRepository.java
@@ -0,0 +1,13 @@
+package com.cooksys.June2020.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+// JpaRepository<Hashtag, Long> to JpaRepository<Hashtable, Long> would remove the error in line 17.
+// JpaRepository<HashTag, Long> written to follow syntax of ChatApp.
+// JpaRepository<HashTag, Long> and not JpaRepository<Hashtag, Long>
+// written to follow syntax in entities package.
+public interface HashtagRepository extends JpaRepository<HashTag, Long> {
+
+}
