fd520fe8e779130c337f9b9bce1fb65fcd68ea21
Edward Morris <ekmorris@outlook.com>
2020-05-15 09:59:02 -0400

    Updated the Service classes.

--- src/main/java/com/cooksys/June2020/services/TagService.java
+++ src/main/java/com/cooksys/June2020/services/TagService.java
@@ -2,7 +2,15 @@ package com.cooksys.June2020.services;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksys.June2020.repositories.HashtagRepository;
+
 @Service
 public class TagService {
+
+	private HashtagRepository hashtagRepository;
+
+	public TagService(HashtagRepository hashtagRepository) {
+		this.hashtagRepository = hashtagRepository;
+	}
 
 }
--- src/main/java/com/cooksys/June2020/services/UserService.java
+++ src/main/java/com/cooksys/June2020/services/UserService.java
@@ -2,7 +2,15 @@ package com.cooksys.June2020.services;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksys.June2020.repositories.UserRepository;
+
 @Service
 public class UserService {
+
+	private UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 }
