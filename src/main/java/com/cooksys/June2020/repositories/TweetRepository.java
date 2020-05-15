8efa20fded3ebba960b1d72b96a0d48da03f7e7d
Matthew Kehrer <matt91.mek@gmail.com>
2020-05-15 14:24:37 -0500

    POST tweets tested & implemented. Close #33

--- src/main/java/com/cooksys/June2020/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/June2020/repositories/TweetRepository.java
@@ -1,11 +1,13 @@
 package com.cooksys.June2020.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cooksys.June2020.entities.Tweet;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Integer> {
-
+    @Query("SELECT t FROM Tweet t WHERE t.id = ?1")
+    Tweet get(Integer id);
 }
